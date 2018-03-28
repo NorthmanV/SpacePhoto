57b44d75a6334c99862ca7a218467af18baa6070
Ruslan Akberov <ruslanakberov@gmail.com>
2018-03-29 00:15:38 +0300

    UI implemented using combination of ScrollView and StackView

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
